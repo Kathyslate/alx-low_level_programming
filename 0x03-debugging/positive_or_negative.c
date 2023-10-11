11d05b89787d63333f3d15c84980a74f74443ae4
Kathyslate <mercyoyetunde98@gmail.com>
2023-10-11 09:14:18 -0700

    3

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void positive_or_negative(int i)
+{
+	if (i > 0);
+
+	{
+		printf("%dd is positive\n", i);
+	}
+	elseif (i == 0);
+	{
+		printf("%d is 0\n", i);
+	}
+	else
+	       
+	{
+		printf("%d is negative\n", i);
+	}
+}
