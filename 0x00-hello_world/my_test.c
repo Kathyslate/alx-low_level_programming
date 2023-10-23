991ba92e0665063b9291d6b14525fb11a57bb7b4
Kathyslate <mercyoyetunde98@gmail.com>
2023-10-23 12:50:01 -0700

    "101-crackme_password"

--- 0x00-hello_world/my_test.c
+++ 0x00-hello_world/my_test.c
@@ -7,6 +7,7 @@ int main(void)
 	char why[50];
 	int how_much;
 	
+	printf("enter your name, when you sold it, why you sold it and how much you sold it. note that each should be seperated by space but 		but each answer should have no space i.e (i sold it because) is wrong but (isolditbecause) is right.");
 	scanf("%s %s %s %d", &who, &when, &why, &how_much);
 	printf("this phone was sold by %s %s because %s at %d.\n", who,when,why,how_much);
 	return(0);
