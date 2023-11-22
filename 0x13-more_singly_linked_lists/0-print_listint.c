a901dce35642aa574a688ad1d97c8259381b382d
kathyslate <mercyoyetunde98@gmail.com>
2023-11-22 00:59:20 -0800

    0x13

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t number = 0;
+	size_t non = 0;
 
-while (h)
+	while (h)
 {
+	non++;
 	printf("%d\n", h->n);
-	number++;
 	h = h->next;
 }
-return (number);
+
+	return (non);
 }
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,14 +9,16 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-unsigned int m = 0;
-listint_t *temp = head;
+	unsigned int m = 0;
 
-while (temp && i < index)
-{
-temp = temp->next;
-m++;
-}
+	for (m < index; m++)
+	{
+		if (head == NULL)
+			return (NULL);
 
-return (temp ? temp : NULL);
+		head = head->next;
+	}
+
+	return (head);
+
 }
